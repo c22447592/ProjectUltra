8359c7c86727d24b31c7199f118797b1aebcc9df
michaelw221 <c22414152@mytudublin.ie>
2024-04-22 17:12:50 +0100

    fixed tilesets and maps
    maps now work again, had to rename the folders to fix tileset links

--- script/global.gd
+++ script/global.gd
@@ -1,14 +1,10 @@
 extends Node
 
-<<<<<<< HEAD
 var player_current_attack = false
 
 var current_scene = 'beach' #initialise variable to spawn beach
-var transition_scene = false #initialise transition state
-=======
 var transition_state = false #initialise transition state
 var scene_destination = 'beach'
->>>>>>> 9add64083822397e6a9b93048c9866a2af75d24c
 
 #initialise player position
 var player_exit_position_x = 0
--- script/npc_village.gd
+++ script/npc_village.gd
@@ -12,30 +12,35 @@ func _process(delta):
 
 
 func _on_beach_entrance_body_entered(body):
+	var global = load("res://script/global.gd").new()
 	if body.has_method("player"):
-		Global.transition_state = true
-		Global.scene_destination = 'beach'
+		global.transition_state = true
+		global.scene_destination = 'beach'
 
 
 func _on_west_entrance_body_entered(body):
+	var global = load("res://script/global.gd").new()
 	if body.has_method("player"):
-		Global.transition_state = true
-		Global.scene_destination = ''
+		global.transition_state = true
+		global.scene_destination = ''
 
 
 func _on_east_entrance_body_entered(body):
+	var global = load("res://script/global.gd").new()
 	if body.has_method("player"):
-		Global.transition_state = true
-		Global.scene_destination = ''
+		global.transition_state = true
+		global.scene_destination = ''
 
 
 func _on_north_entrance_body_entered(body):
+	var global = load("res://script/global.gd").new()
 	if body.has_method("player"):
-		Global.transition_state = true
-		Global.scene_destination = ''
+		global.transition_state = true
+		global.scene_destination = ''
 
 func change_scene():
-	if Global.transition_state == true:
-		if Global.scene_destination == 'beach':
+	var global = load("res://script/global.gd").new()
+	if global.transition_state == true:
+		if global.scene_destination == 'beach':
 			get_tree().change_scene_to_file("res://scenes/beach.tscn")
-			Global.finish_transition()
+			global.finish_transition()
