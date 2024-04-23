54378f6a8fdfbdb433fd23381c58458dd95e0b6e
michaelw221 <c22414152@mytudublin.ie>
2024-04-24 00:53:59 +0100

    finished transistions between scenes

--- script/bossMap1.gd
+++ script/bossMap1.gd
@@ -10,16 +10,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-
-func _on_area_2d_body_entered(body):
-	if body.has_method("player"):
-		Global.transition_state = true
-		Global.scene_destination = 'npc_village'
-		change_scene()
-
-func change_scene():
-	if Global.transition_state == true:
-		if Global.scene_destination == 'npc_village':
-			get_tree().change_scene_to_file("res://scenes/npc_village.tscn")
-			Global.finish_transition()
