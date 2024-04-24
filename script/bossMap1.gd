34fc4f05733fc9cb8b4c594506a3d3870ea4013b
michaelw221 <c22414152@mytudublin.ie>
2024-04-24 18:43:00 +0100

    Music added

--- script/bossMap1.gd
+++ script/bossMap1.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var audio_file1 = $AudioStreamPlayer2D
+@onready var audio_file2 = $AudioStreamPlayer2D2
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,3 +11,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("player"):
+		audio_file1.play()
+		audio_file2.stop()
+	else:
+		audio_file2.play()
+		audio_file1.stop()
