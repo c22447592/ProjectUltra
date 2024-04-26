6caf227b3e1ebe4ad0a2f6f6190dab2fe2bab9cd
Johnm56 <C22789271@mytudublin.ie>
2024-04-26 11:53:32 +0100

    Inventory Npc village

--- script/npc_village.gd
+++ script/npc_village.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 @onready var game_menu = $CanvasLayer/game_menu as gameMenu
+@onready var heartContainer=$CanvasLayer/HBoxContainer
+@onready var player=$thePlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	heartContainer.setMaxHearts(10 )
+	heartContainer.updateHearts(GameData.player_health)
+	player.healthChanged.connect(heartContainer.updateHearts)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +23,11 @@ func _input(event):
 		elif game_menu.visible == false: ##closed
 			game_menu.visible = true
 
+
+
+func _on_inventory_closed():
+	get_tree().paused=false
+
+
+func _on_inventory_opened():
+	get_tree().paused=true
